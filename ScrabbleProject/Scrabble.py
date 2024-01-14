d24cb7dd1f789b96cb0f6d2b481f65ebfd1cc896
Bodescu Stefan Rrares <100295422+Stefan1811@users.noreply.github.com>
2024-01-14 22:57:51 +0200

    score_calculation_solved

--- ScrabbleProject/Scrabble.py
+++ ScrabbleProject/Scrabble.py
@@ -9,6 +9,10 @@ class Player:
         self.score = 0
 
 class ScrabbleGame:
+    dl=[(0, 3), (0, 11), (2, 6), (2, 8), (3, 0), (3, 7), (3, 14), (6, 2), (6, 6), (6, 8),(6, 12), (7, 3), (7, 11), (8, 2), (8, 6), (8, 8), (8, 12), (11, 0), (11, 7), (11, 14),(12, 6), (12, 8), (14, 3), (14, 11)]
+    tl=[(1, 5), (1, 9), (5, 1), (5, 5), (5, 9), (5, 13), (9, 1), (9, 5), (9, 9), (9, 13),(13, 5), (13, 9)]
+    dw=[(1, 1), (2, 2), (3, 3), (4, 4), (10, 10), (11, 11), (12, 12), (13, 13),(1, 13), (2, 12), (3, 11), (4, 10), (10, 4), (11, 3), (12, 2), (13, 1),(7, 7)]
+    tw=[(0, 0), (0, 7), (0, 14), (7, 0), (7, 14), (14, 0), (14, 7), (14, 14)]
     def __init__(self):
         self.root = tk.Tk()
         self.root.title("Scrabble Game")
@@ -21,7 +25,7 @@ class ScrabbleGame:
         self.players = [Player("Player 1"), Player("Player 2")]
         self.racks = [self.draw_tiles(7), self.draw_tiles(7)]  
         self.played_letters = []
-        self.played_words = []  # Lista pentru a tine evidenta cuvintelor deja plasate
+        self.played_words = []  
         self.letter_scores = {
             'A': 1, 'B': 3, 'C': 3, 'D': 2, 'E': 1, 'F': 4, 'G': 2, 'H': 4, 'I': 1,
             'J': 8, 'K': 5, 'L': 1, 'M': 3, 'N': 1, 'O': 1, 'P': 3, 'Q': 10, 'R': 1,
@@ -29,7 +33,7 @@ class ScrabbleGame:
         }
         self.word_list = self.load_word_list("dictionary.txt")
         self.create_widgets()
-
+       
     def switch_player(self):
         self.current_player = self.players[1] if self.current_player == self.players[0] else self.players[0]
 
@@ -74,17 +78,13 @@ class ScrabbleGame:
                 cell = tk.Label(self.board_frame, text=self.board[i][j], width=2, height=1, relief="ridge", borderwidth=2)
                 cell.grid(row=i, column=j, padx=1, pady=1, ipadx=5, ipady=5)
                 cell.bind("<Button-1>", lambda event, row=i, col=j: self.cell_clicked(row, col))
-                if (i, j) in [(0, 0), (0, 7), (0, 14), (7, 0), (7, 14), (14, 0), (14, 7), (14, 14)]:
+                if (i, j) in self.tw:
                      cell.configure(bg="red")  #triplu cuvant
-                elif (i, j) in [(1, 1), (2, 2), (3, 3), (4, 4), (10, 10), (11, 11), (12, 12), (13, 13),
-                            (1, 13), (2, 12), (3, 11), (4, 10), (10, 4), (11, 3), (12, 2), (13, 1),(7, 7)]:
+                elif (i, j) in self.dw:
                      cell.configure(bg="pink")  #dublu cuvant
-                elif (i, j) in [(1, 5), (1, 9), (5, 1), (5, 5), (5, 9), (5, 13), (9, 1), (9, 5), (9, 9), (9, 13),
-                            (13, 5), (13, 9)]:
+                elif (i, j) in self.tl:
                      cell.configure(bg="blue")  #triplu literă
-                elif (i, j) in [(0, 3), (0, 11), (2, 6), (2, 8), (3, 0), (3, 7), (3, 14), (6, 2), (6, 6), (6, 8),
-                             (6, 12), (7, 3), (7, 11), (8, 2), (8, 6), (8, 8), (8, 12), (11, 0), (11, 7), (11, 14),
-                            (12, 6), (12, 8), (14, 3), (14, 11)]:
+                elif (i, j) in self.dl:
                      cell.configure(bg="lightblue")  #dublu litera
                 
 
@@ -113,6 +113,7 @@ class ScrabbleGame:
         self.draw_new_tiles(7)
         self.total_letters -= 7
         self.update_rack()
+        self.total_letters += 7
         self.remaining_letters_label.config(text=f"Remaining Letters: {self.total_letters}")
 
     def load_word_list(self, file_path="dictionary.txt"):
@@ -133,7 +134,8 @@ class ScrabbleGame:
             if self.place_word_on_board(word):
                 score = self.calculate_score(word)
                 self.current_player.score += score
-                self.score_label.config(text=f"{self.players[0].name}'s Score: {self.players[0].score} | {self.players[1].name}'s Score: {self.players[1].score}")
+                self.score_label.config(
+                    text=f"{self.players[0].name}'s Score: {self.players[0].score} | {self.players[1].name}'s Score: {self.players[1].score}")
                 messagebox.showinfo("Word Played", f"{self.current_player.name} played the word '{word}' with a score of {score}")
                 self.clear_rack()
                 self.draw_new_tiles(len(word))
@@ -141,7 +143,7 @@ class ScrabbleGame:
                 self.remaining_letters_label.config(text=f"Remaining Letters: {self.total_letters}")
                 self.update_rack()
                 self.played_letters = []
-                self.played_words.append(word)  # Adaugă cuvântul în lista cuvintelor deja plasate
+                self.played_words.append(word) 
                 self.switch_player()
             else:
                 messagebox.showwarning("Invalid Placement", "The word cannot be placed on the board.")
@@ -154,7 +156,6 @@ class ScrabbleGame:
     def is_word_valid(self, word):
         return all(letter in self.racks[0] + self.played_letters + self.played_words for letter in word)
 
-   
 
     def draw_tiles(self, num_tiles):
         tiles = []
@@ -200,12 +201,30 @@ class ScrabbleGame:
         return True
     
     def calculate_score(self, word):
-        score = 0
-        for letter in word:
-            if letter.isalpha():
-                score += self.letter_scores.get(letter.upper(), 0)
-        return score
-    
+     score = 0
+     word_multiplier = 1
+
+     for letter in word:
+        if letter.isalpha():
+           for i in range(15):
+            for j in range(15):
+                if self.board[i][j] == letter:
+                    if (i, j) in self.tl:
+                        score += self.letter_scores.get(letter.upper(), 0) * 3
+                    elif (i, j) in self.dl:
+                        score += self.letter_scores.get(letter.upper(), 0) * 2
+                    else:
+                        score += self.letter_scores.get(letter.upper(), 0)
+
+                    if (i, j) in self.tw:
+                        word_multiplier *= 3
+                    elif (i, j) in self.dw:
+                        word_multiplier *= 2
+
+     score *= word_multiplier
+     return score
+
+
     def update_board(self):
         for i in range(15):
             for j in range(15):
@@ -224,3 +243,5 @@ class ScrabbleGame:
 if __name__ == "__main__":
     scrabble_game = ScrabbleGame()
     scrabble_game.run()
+    
+    
